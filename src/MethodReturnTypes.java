37c535c8e444bef09dafc7900358435131b91bc6
ekdrobinski <127761998+ekdrobinski@users.noreply.github.com>
2023-05-11 08:21:00 -0500

    Update MethodReturnTypes.java

--- src/MethodReturnTypes.java
+++ src/MethodReturnTypes.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MethodReturnTypes {
+public class MethodReturnTypes {
+    public static void main(String[] args) {
+        System.out.println("This returns a string:  " + getString());
+        System.out.println("This returns an integer:  " + getInteger());
+        System.out.println("This returns a boolean value:  " + getBoolean());
+    }
+    public static String getString() {
+        return "This returns a string";
+    }
+    public static int getInteger() {
+        return 714;
+    }
+    public static boolean getBoolean() {
+        return false;
+    }
 }
