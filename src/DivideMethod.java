87f41c1bcd2451923cf611ecd2d9ca508af5b1ff
ekdrobinski <127761998+ekdrobinski@users.noreply.github.com>
2023-05-11 08:21:48 -0500

    Update DivideMethod.java

--- src/DivideMethod.java
+++ src/DivideMethod.java
@@ -1,17 +1,19 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Scanner;
+public class DivideMethod {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number to divide: ");
+        double firstNum = scanner.nextInt();
+        System.out.print("Enter the second number to divide by ");
+        double secondNum = scanner.nextInt();
+        divide(firstNum, secondNum);
+    }
+    public static void divide(double firstNum, double secondNum) {
+        if (secondNum != 0) {
+            double answer = firstNum / secondNum;
+            System.out.println("The quotient(end result) is " + answer + ".");
+        } else {
+            System.out.println("Undefined. Bruh, you can't divide by 0");
         }
     }
 }
